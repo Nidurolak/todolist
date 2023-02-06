f44bdec054129d492994b1ba353b52ebe88af8b4
Nidurolak <jaewoon08@gmail.com>
2023-02-06 21:11:41 +0900

    아이고 맙소사

--- src/App.js
+++ src/App.js
@@ -4,26 +4,75 @@ import './App.css';
 
 
 function ShowToDo(props){
+  const buttonString = props.todo.isComplete == false ?"완료" :"취소"
   return(
     <div className="todobox">
-      <h2>{props.name}</h2>
-      <div>{props.comment}</div>
+      <h2>{props.todo.todoName}</h2>
+      <div>{props.todo.todoContents}</div>
       <div className="todobuttoncontainer">
-        <button className="todoboxcompleteordelete"><b>완료</b></button>
-        <button className="todoboxcompleteordelete"><b>삭제</b></button>
+        <button className="todoboxcompleteordelete" onClick={() => props.completeOrUndoToDo(props.todo.id)}><b>{buttonString}</b></button>
+        <button className="todoboxcompleteordelete" onClick={() => props.todoDelete(props.todo.id)}><b>{props.todo.id}</b></button>
       </div>
     </div>
   )}
 
 function App() {
 
-  const [todo, settodo] = useState([
-    {id: 0, name: '첫 번째 할일', comment: '리액트 완성', iscomplite: false},
-    {id: 0, name: '첫 번째 할일', comment: '리액트 완성', iscomplite: false},
-    {id: 0, name: '첫 번째 할일', comment: '리액트 완성', iscomplite: false},
-
+  const [todo, setTodo] = useState([
+    {id: 0, todoName: '첫 번째 할일', todoContents: '리액트 완성', isComplete: false},
+    {id: 1, todoName: '첫 번째 할일', todoContents: 'wk', isComplete: false},
+    {id: 2, todoName: '성공한 일', todoContents: '점심 먹기', isComplete: true},
   ])
+  const[todoName, setName] = useState('')
+  const[todoContents, setComment] = useState('')
+  const[isComplete, setIscomplete] = useState(false)
 
+  const addTodoList = () =>{
+    const newTodo = {
+      id : todo[todo.length -1].id + 1,
+      todoName : todoName,
+      todoContents : todoContents,
+      isComplete : false
+    }
+    setTodo([...todo, newTodo])
+  }
+
+  const nameChangeHandler = (event) =>{
+    setName(event.target.value)
+  }
+  const commentChangeHandler = (event) =>{
+    setComment(event.target.value)
+  }
+  const completeOrUndoToDo = (id) => {
+    //const newTodo = todo.map((list) => {if(list.id == id){list.isComplete ==false ?list.isComplete = true :list.isComplete = false}})
+    //console.log(newTodo)
+    //인덱스값이 id가 아니라 id값을 가진 인덱스를 찾아야해
+    //const bool = todo[todo.find(index => index.id == id)].isComplete == true ?todo[id].isComplete = false :todo[id].isComplete = true;
+    //console.log(todo.find(index => index.id == id));
+    //const ID =todo.find(index => index.id == id)
+    const bool = todo[id].isComplete == true ?todo[id].isComplete = false :todo[id].isComplete = true;
+    setIscomplete(() => {
+      const newTodo = {...todo};
+      newTodo[id].isComplete = bool;
+      return newTodo
+    })
+      /*if(todo[id].isComplete === false){
+        console.log( 'affafa')
+        todo[id].isComplete = true
+        console.log(todo[id])
+      }
+      else{
+        console.log("zzzzzz")
+        todo[id].isComplete = false
+        console.log(todo[id])
+      }*/
+      //setTodo([...todo, null])
+  }
+
+  const todoDelete = (id) =>{
+    const newTodo = todo.filter((todo) => todo.id !== id)
+    setTodo(newTodo)
+  }
 
   return (<>
     <div className='defaultlayout'>
@@ -34,30 +83,28 @@ function App() {
       <div className='titlecontainer'>
         <div className = "submitwrapper">
         <div className="submitbox">
-        제목  <input />
+        제목  <input value={todoName} onChange={nameChangeHandler}/>
         </div>
         <div className="submitbox">
-        내용  <input />
+        내용  <input value={todoContents} onChange={commentChangeHandler}/>
         </div>
         </div>
-        <button className='submitbutton'><b>기록하기</b></button>
+        <button className='submitbutton' onClick={addTodoList}>
+          
+          <b>기록하기</b></button>
       </div>
       <div className='todocontainer'>
         <div><h2>하는 중......</h2></div>
         <div className="todoboxcontainer">
-          {todo.map((list) => { return(ShowToDo(list))})}
+          {todo.filter((list) => list.isComplete === false).map((list) =>
+          { return(<ShowToDo key = {list.id} todo={list} completeOrUndoToDo = {completeOrUndoToDo} todoDelete ={todoDelete}/>)})}
         </div>
         
         <div><h2>끝난 것!</h2></div>
         <div className="todoboxcontainer">
-          <div className="todobox">
-            <h2>하는 중</h2>
-            <div>asdasddasadsaasadsadadda</div>
-            <div className="todobuttoncontainer">
-              <button className={"todoboxcompleteordelete"}>완료</button>
-              <button className={"todoboxcompleteordelete"}>삭제</button>
-            </div>
-          </div>
+          
+          {todo.filter((list) => list.isComplete === true).map((list) =>
+          { return(<ShowToDo key = {list.id} todo={list} completeOrUndoToDo = {completeOrUndoToDo} todoDelete ={todoDelete}/>)})}
         </div>
       </div>
     </div>
