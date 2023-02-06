4a64f66288b157837f1931050a869aaa6499ef44
Nidurolak <jaewoon08@gmail.com>
2023-02-06 22:22:34 +0900

    이정도면 꽤 쌔끈하게 완성된 거 같은데?

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function ShowToDo(props){
       <div>{props.todo.todoContents}</div>
       <div className="todobuttoncontainer">
         <button className="todoboxcompleteordelete" onClick={() => props.completeOrUndoToDo(props.todo.id)}><b>{buttonString}</b></button>
-        <button className="todoboxcompleteordelete" onClick={() => props.todoDelete(props.todo.id)}><b>{props.todo.id}</b></button>
+        <button className="todoboxcompleteordelete" onClick={() => props.todoDelete(props.todo.id)}><b>삭제하기</b></button>
       </div>
     </div>
   )}
@@ -19,43 +19,52 @@ function ShowToDo(props){
 function App() {
 
   const [todo, setTodo] = useState([
-    {id: 0, todoName: '첫 번째 할일', todoContents: '리액트 완성', isComplete: false},
-    {id: 1, todoName: '첫 번째 할일', todoContents: 'wk', isComplete: false},
-    {id: 2, todoName: '성공한 일', todoContents: '점심 먹기', isComplete: true},
+    {id: 0, todoName: '첫 번째 할 일', todoContents: '리액트 완성', isComplete: false},
+    {id: 1, todoName: '두 번째 할 일', todoContents: '임창정 소주한잔 완창하기', isComplete: false},
+    {id: 2, todoName: '세 번째 할 일', todoContents: '뭐하지', isComplete: true},
   ])
   const[todoName, setName] = useState('')
   const[todoContents, setComment] = useState('')
   const[isComplete, setIscomplete] = useState(false)
 
   const addTodoList = () =>{
-    const newTodo = {
-      id : todo[todo.length -1].id + 1,
-      todoName : todoName,
-      todoContents : todoContents,
-      isComplete : false
+    if(todoName != '' && todoContents != ''){
+      const newTodo = {
+        id : todo[todo.length -1].id + 1,
+        todoName : todoName,
+        todoContents : todoContents,
+        isComplete : false
+      }
+      setTodo([...todo, newTodo])
+      setName('')
+      setComment('')
     }
-    setTodo([...todo, newTodo])
   }
 
   const nameChangeHandler = (event) =>{
-    setName(event.target.value)
+    if(event.target.value !=''){
+      setName(event.target.value)
+    }
   }
   const commentChangeHandler = (event) =>{
-    setComment(event.target.value)
+    if(event.target.value != ''){
+      setComment(event.target.value)
+    }
+    
   }
   const completeOrUndoToDo = (id) => {
     //const newTodo = todo.map((list) => {if(list.id == id){list.isComplete ==false ?list.isComplete = true :list.isComplete = false}})
     //console.log(newTodo)
     //인덱스값이 id가 아니라 id값을 가진 인덱스를 찾아야해
     //const bool = todo[todo.find(index => index.id == id)].isComplete == true ?todo[id].isComplete = false :todo[id].isComplete = true;
-    //console.log(todo.find(index => index.id == id));
-    //const ID =todo.find(index => index.id == id)
-    const bool = todo[id].isComplete == true ?todo[id].isComplete = false :todo[id].isComplete = true;
+    const ID =todo.find(index => index.id == id)
+    const bool = ID.isComplete == true ?ID.isComplete = false :ID.isComplete = true;
+    const newTodo = {...todo};
+    ID.isComplete = bool;
     setIscomplete(() => {
-      const newTodo = {...todo};
-      newTodo[id].isComplete = bool;
       return newTodo
     })
+    //이렇게 하지 말고 셋 스테이트 밖에 스프레드 문법을 써라
       /*if(todo[id].isComplete === false){
         console.log( 'affafa')
         todo[id].isComplete = true
