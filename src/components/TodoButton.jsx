fecf83afc301dabb6456268bea701a903d3bcac1
Nidurolak <jaewoon08@gmail.com>
2023-02-08 21:25:58 +0900

    컴포넌트 분리 연습, 경로잡는 게 조금 어색했다.

--- src/components/TodoButton.jsx
+++ src/components/TodoButton.jsx
@@ -2,7 +2,6 @@ function TodoButton(props) {
     return (
         <button className="todoboxcompleteordelete" onClick={() => props.todoDelete(props.todo.id)}><b>삭제하기</b></button>
     )
-
 }
 
 export default TodoButton
